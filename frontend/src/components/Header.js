7967732d784bdc66a5d900ce516f1d1dd59061e8
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-07 17:37:28 +1300

    Created first item

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,9 +4,12 @@ class Header extends React.Component {
 
   render() {
     return(
-      <header class='row'>
-        <div>
-          <a href='index.html' class='brand'>Tronicom</a>
+      <header className='row'>
+        <div className='brand'>
+          <span>
+            <i className='fa fa-music'></i>
+          </span>
+          <a href='index.html'>Tronicom</a>
         </div>
         <div>
           <a href='cart.html'>Cart</a>
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,25 +5,25 @@ class Main extends React.Component {
   render() {
     return(
       <main>
-        <div>
-
-
-        </div>
-        <div className='product'>
-          <a href="product.html">
-            <img src='./images/product-1.jpg' alt='product'></img>
-          </a>
-          <div className='product-body'>
-            <a href='product.html'>
-              <h2>Worlds</h2>
+        <div className='row center'>
+          <div className='card'>
+            <a href="product.html">
+              <img className='medium' src='./images/p1.jpg' alt='product'></img>
             </a>
-            <div class='rating'>
-              <span>
-                <i class='fa fa-start'></i>
-              </span>
-            </div>
-            <div class='price'>
-              120
+            <div className='card-body'>
+              <a href='product.html'>
+                <h2>Worlds</h2>
+              </a>
+              <div className='rating'>
+                <span><i className='fa fa-star'></i></span>
+                <span><i className='fa fa-star'></i></span>
+                <span><i className='fa fa-star'></i></span>
+                <span><i className='fa fa-star'></i></span>
+                <span><i className='fa fa-star'></i></span>
+              </div>
+              <div className='price'>
+                17.99
+              </div>
             </div>
           </div>
         </div>
