127ceb52352450be11aa66103ec474a021b24553
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-08 15:21:25 +1300

    Created basic product screen

--- frontend/src/routes/ProductScreen.js
+++ frontend/src/routes/ProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from '../components/Rating';
 import data from '../data';
 
@@ -9,30 +10,57 @@ export default function ProductScreen(props) {
   }
 
   return(
-    <div className='row'>
-      <div className='col-2'>
-        <img className='large' src={product.image} alt = {product.name}></img>
-      </div>
-      <div className='col-1'>
-        <ul>
-          <li>
-            <h1>{product.name}</h1>
-          </li>
-          <li>
-            <Rating>
-              rating = {product.rating}
-              numReviews = {product.numReviews}
-            </Rating>
-          </li>
-          <li>
-            Price : ${product.price}
-          </li>
-        </ul>
-      </div>
-      <div className='col-1'>
+    <div>
+      <Link to='/'>Back to results</Link>
+      <div className='row top'>
+        <div className='col-2'>
+          <img className='large' src={product.image} alt = {product.name}></img>
+        </div>
+        <div className='col-1'>
+          <ul>
+            <li>
+              <h1>{product.name}</h1>
+            </li>
+            <li>
+              Artist: {product.artist}
+            </li>
+            <li>
+              <Rating rating={product.rating} numReviews={product.numReviews} />
+            </li>
+            <li>
+              Price : ${product.price}
+            </li>
+          </ul>
+        </div>
+        <div className='col-1'>
+          <div className='card card-body'>
+            <ul>
+              <li>
+                <div className='row'>
+                  <div>Price</div>
+                  <div className='price'>${product.price}</div>
+                </div>
+              </li>
+              <li>
+                <div className='row'>
+                  <div>Status</div>
+                  <div>
+                    {product.stock > 0 ? (
+                      <span className='inStock'>In stock</span>    
+                    ): (  
+                      <span className='outStock'>Unavailable</span>
+                    )}
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button className='add'>Add to Cart</button>
+              </li>
+            </ul>
+          </div>
+        </div>
 
       </div>
-
     </div>
   );
 }
