d39d62c526bb5f98b261d161c11423dbb250fabf
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-08 17:04:43 +1300

    Adding comments

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
-// Extension is required for import
+
+// File extensions are required for backend import
 import data from './data.js';
 
+// Express server is initialised
 const app = express();
+
+// Port is either environment variable PORT or is by default 5000
 const port = process.env.PORT || 5000;
 
+// Returns the product database
 app.get('/api/products', (req, res) => {
   res.send(data.products)
 });
@@ -13,6 +18,7 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+// Prints to console what port the server is 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
