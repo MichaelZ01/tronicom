ee509b58bc9a7e80bb2910801530c252a6f441af
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-09 11:21:50 +1300

    Added custom loader

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -5,6 +5,12 @@ export default function LoadingBox() {
   return (
     <div>
       <i className='fa fa-spinner fa-spin'></i> Loading...
+      <div id='loader-wrapper'>
+        <div id='loader'></div>
+        <div className='loader-section section-left'></div>
+        <div className='loader-section section-right'></div>
+      </div>
     </div>
+
   )
 }
