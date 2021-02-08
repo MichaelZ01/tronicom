c17c60e1e25e686a57ba724507d232fb87a0d0d5
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-08 16:06:23 +1300

    Connected frontend and backend using axios

--- frontend/src/routes/HomeScreen.js
+++ frontend/src/routes/HomeScreen.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Product from '../components/Product';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+
+  // React hook: Default value: [], Use setProducts to change state
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const { data } = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch(err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [])
+
   return(
-    <div className='row center'>
-      {data.products.map((product) => (
-        <Product key={product._id} product={product} />
-      ))}
+    <div>
+      {loading ? ( <LoadingBox></LoadingBox>
+      ) : error ? ( <MessageBox varient='error'>{error}</MessageBox>
+      ) : (
+        <div className='row center'>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )} 
     </div>
+
   );
 }
