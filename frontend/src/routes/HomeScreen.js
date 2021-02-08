23e5064a6a7793ab1525b52b81362c6a0a6cb508
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-09 11:34:37 +1300

    Added documentation

--- frontend/src/routes/HomeScreen.js
+++ frontend/src/routes/HomeScreen.js
@@ -6,16 +6,28 @@ import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
 
-  // React hook: Default value: [], Use setProducts to change state
   /*
-  ADD COMMENT
-
+  React hook:
+  - allow you to use React without classes
+  - useState: State Hook -> 
+      Similar to a state class with a constructor state and this.setState
+  - useEffect: Effect Hook -> 
+      (componentDidMount, componentDidUpdate, componentWillUnmount)
   */
+
+  // Declare new state variables 
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+
+  // Similar to componentDidMount and componentDidUpdate
   useEffect(() => {
+
+    // Async is just special syntax for promises
     const fetchData = async () => {
+
+      // Attempt to retrieve data from backend
       try {
         setLoading(true)
         const { data } = await axios.get('/api/products');
