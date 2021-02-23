b4f1c4930be8644fd20814f97c049a04e6c6a417
Michael <48680828+MichaelZ01@users.noreply.github.com>
2021-02-24 01:49:45 +1300

    Fixed typo bug

--- frontend/src/routes/OrderScreen.js
+++ frontend/src/routes/OrderScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
 import CheckoutSteps from '../components/CheckoutSteps';
 import { createOrder } from '../redux/actions';
 import { ORDER_CREATE_RESET } from '../redux/actionTypes';
